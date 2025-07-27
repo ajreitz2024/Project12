f1e0aaa921521868498f732aed8bc1bca367b232
ajreitz2024 <ajreitz2024@gmail.com>
2025-07-27 16:37:05 -0400

    Added Vehicle.h class file, defined the base class Vehicle, and added the member variables, constructor, getters, setters, and a function to display the information of the object.

---
+++ Project12/Vehicle.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle
+{
+private:
+	string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle(string manufacturerInfo, int yearInfo) { // constructor
+		manufacturer = manufacturerInfo;
+		yearBuilt = yearInfo;
+	}
+	void setManufacturer(string manufacturerInfo) { // setter for manufacturer
+		manufacturer = manufacturerInfo;
+	}
+	void setYear(int yearInfo) { // setter for year built
+		yearBuilt = yearInfo;
+	}
+	string getManufacturer() { // getter for manufacturer
+		return manufacturer;
+	}
+	int getYear() { // getter for year built
+		return yearBuilt;
+	}
+	void displayInfo(string manufacturer, int yearBuilt) { // displays all info from class
+		cout << "Vehicle Information:" << endl;
+		cout << "Manufacturer: " << manufacturer << endl;
+		cout << "Year Built: " << yearBuilt << endl;
+	}
+};
+
+#endif
